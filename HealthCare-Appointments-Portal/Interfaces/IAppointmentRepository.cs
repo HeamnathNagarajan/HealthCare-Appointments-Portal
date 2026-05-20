453ee9f7f83232e86fe77f8b38ee055c29c74594
Heamnath <heamnathnagarajan@gmail.com>
2026-05-20 15:49:20 +0530

    Implemented repository layer using in-memory datastore

---
+++ HealthCare-Appointments-Portal/Interfaces/IAppointmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using HealthCare_Appointment_Portal.Models;
+
+namespace HealthCare_Appointment_Portal.Interfaces
+{
+
+    public interface IAppointmentRepository
+    {
+
+        void AddAppointment(Appointment appointment);
+
+        Appointment? GetAppointmentById(Guid appointmentId);
+
+        List<Appointment> GetAllAppointments();
+
+        void UpdateAppointment(Appointment updatedAppointment);
+
+        void DeleteAppointmentById(Guid appointmentId);
+    }
+}
---
+++ HealthCare-Appointments-Portal/Interfaces/IHealthRecordRepository.cs
@@ -0,0 +1,19 @@
+﻿using HealthCare_Appointment_Portal.Models;
+
+namespace HealthCare_Appointment_Portal.Interfaces
+{
+
+    public interface IHealthRecordRepository
+    {
+
+        void AddRecord(HealthRecord record);
+
+        List<HealthRecord> GetAllRecords();
+
+        HealthRecord? GetRecordById(Guid recordId);
+
+        void UpdateRecord(HealthRecord updatedRecord);
+
+        void DeleteRecordById(Guid recordId);
+    }
+}

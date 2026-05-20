406288e08273e3349145dfef184c720d40e55b83
Heamnath <heamnathnagarajan@gmail.com>
2026-05-20 15:51:08 +0530

    Implemented patient,doctor ,appointment and health record services with validations

---
+++ HealthCare-Appointments-Portal/Interfaces/IHealthRecordService.cs
@@ -0,0 +1,36 @@
+﻿using HealthCare_Appointment_Portal.Models;
+
+namespace HealthCare_Appointment_Portal.Interfaces
+{
+
+    public interface IHealthRecordService
+    {
+
+        // Add New Health Record
+        void AddRecord(
+            HealthRecord record);
+
+        // Get Health Record By Id
+        HealthRecord? GetRecordById(
+            Guid recordId);
+
+        // Get All Health Records
+        List<HealthRecord> GetAllRecords();
+
+        // Get Records By Patient
+        List<HealthRecord> GetRecordsByPatient(
+            Guid patientId);
+
+        // Get Records By Doctor
+        List<HealthRecord> GetRecordsByDoctor(
+            Guid doctorId);
+
+        // Update Existing Health Record
+        void UpdateRecord(
+            HealthRecord updatedRecord);
+
+        // Delete Health Record By Id
+        void DeleteRecordById(
+            Guid recordId);
+    }
+}
---
+++ HealthCare-Appointments-Portal/Services/DoctorService .cs
@@ -0,0 +1,136 @@
+﻿using HealthCare_Appointment_Portal.Enums;
+using HealthCare_Appointment_Portal.Exceptions;
+using HealthCare_Appointment_Portal.Interfaces;
+using HealthCare_Appointment_Portal.Models;
+
+namespace HealthCare_Appointment_Portal.Services
+{
+
+    public class DoctorService : IDoctorService
+    {
+
+        private readonly IDoctorRepository _doctorRepository;
+
+        // Dependency Injection
+        public DoctorService(
+            IDoctorRepository doctorRepository)
+        {
+
+            _doctorRepository = doctorRepository;
+        }
+
+        // Add New Doctor
+        public void AddDoctor(Doctor doctor)
+        {
+
+            Doctor? existingDoctor =
+                _doctorRepository
+                .GetAllDoctors()
+                .FirstOrDefault(d =>
+                    d.FullName == doctor.FullName &&
+                    d.Specialisation ==
+                    doctor.Specialisation);
+
+            if (existingDoctor != null)
+            {
+
+                throw new DuplicateDoctorException();
+            }
+
+            _doctorRepository.AddDoctor(doctor);
+        }
+
+        // Get Doctor By Id
+        public Doctor? GetDoctorById(
+            Guid doctorId)
+        {
+
+            Doctor? doctor =
+                _doctorRepository
+                .GetDoctorById(doctorId);
+
+            if (doctor == null)
+            {
+
+                throw new DoctorNotFoundException();
+            }
+
+            return doctor;
+        }
+
+        // Get All Doctors
+        public List<Doctor> GetAllDoctors()
+        {
+
+            return _doctorRepository
+                .GetAllDoctors();
+        }
+
+        // Search Doctors By Specialisation
+        public List<Doctor>
+            GetDoctorsBySpecialisation(
+                Specialisation specialisation)
+        {
+
+            return _doctorRepository
+                .GetAllDoctors()
+                .Where(d =>
+                    d.Specialisation ==
+                    specialisation)
+                .ToList();
+        }
+
+        // Get Available Doctors By Specialisation
+        public List<Doctor> GetAvailableDoctorsBySpecialisation(
+                Specialisation specialisation)
+        {
+
+            return _doctorRepository
+                .GetAllDoctors()
+                .Where(d =>
+                    d.Specialisation ==
+                    specialisation &&
+                    d.IsActive)
+                .ToList();
+        }
+
+        // Update Existing Doctor
+        public void UpdateDoctor(
+            Doctor updatedDoctor)
+        {
+
+            Doctor? existingDoctor =
+                _doctorRepository
+                .GetDoctorById(
+                    updatedDoctor.DoctorId);
+
+            if (existingDoctor == null)
+            {
+
+                throw new DoctorNotFoundException();
+            }
+
+            _doctorRepository
+                .UpdateDoctor(updatedDoctor);
+        }
+
+        // Delete Doctor By Id
+        public void DeleteDoctorById(
+            Guid doctorId)
+        {
+
+            Doctor? doctor =
+                _doctorRepository
+                .GetDoctorById(doctorId);
+
+            if (doctor == null)
+            {
+
+                throw new DoctorNotFoundException();
+            }
+
+            _doctorRepository
+                .DeleteDoctorById(doctorId);
+        }
+    }
+}
